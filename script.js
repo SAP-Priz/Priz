440336a641478043676c84b923215aed0f006b2c
sambee1 <nnajisamuel5@gmail.com>
2023-07-11 12:05:04 +0100

    Next commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const transactionDetail = document.querySelectorAll(
+  ".transaction-detail-modal"
+);
+const modalBtn = document.querySelector(".transaction-detail-modal-btnClose");
+console.log(transactionDetail);
+for (let i = 0; i < transactionDetail.length; i++) {
+  transactionDetail[i].addEventListener("click", function () {
+    // transactionDetail[i].classList.remove(".hidden");
+    console.log(transactionDetail[i]);
+  });
+}
+modalBtn.addEventListener("click", function () {
+  console.log("clicked");
+  alert("CLicked");
+});
